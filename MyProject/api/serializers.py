17be07caf1a82146a1ac4be85e01d2c6889821bc
Sowndharya Karthik <sowndharyakrishnan@gmail.com>
2022-06-26 18:31:24 +0530

    seri

--- MyProject/api/serializers.py
+++ MyProject/api/serializers.py
@@ -2,6 +2,8 @@ from dataclasses import fields
 from rest_framework import serializers
 from baseApp.models import *
 # this is for serizlizer
+
+# made from user
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
