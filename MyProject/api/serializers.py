50ed97ab9e49173d802940504bf81a7975d2bba6
Sowndharya0407 <104989423+Sowndharya0407@users.noreply.github.com>
2022-06-26 15:46:55 +0530

    Update serializers.py

--- MyProject/api/serializers.py
+++ MyProject/api/serializers.py
@@ -1,7 +1,7 @@
 from dataclasses import fields
 from rest_framework import serializers
 from baseApp.models import *
-
+# this is for serizlizer
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
